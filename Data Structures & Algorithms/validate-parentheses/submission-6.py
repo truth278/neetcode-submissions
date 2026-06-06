dec62c6900ef89997870a0a58245d082675666c6
truth278 <harbinger.of.truth720@gmail.com>
2026-06-06 22:45:05 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lost = []
+        q = ["(", "{", "["]
+        for p in s:
+            if p in q:
+                lost.append(p)
+                continue
+            if not lost:
+                return False
+            if p == "}":
+                if lost[-1] == "{":
+                    lost.pop()
+                else:
+                    return False
+            elif p == ")":
+                if lost[-1] == "(":
+                    lost.pop()
+                else:
+                    return False
+            else:
+                if lost[-1] == "[":
+                    lost.pop()
+                else:
+                    return False
+                    
+        return len(lost) == 0
